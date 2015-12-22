84268084972679ceb16cc32311dd40a4dd3a1f17
Mohamed El Mouctar HAIDARA <mohamed.haidara@insa-lyon.fr>
2015-12-22 22:12:24 +0100

    New function for Rbt. In progress

--- src/Rbt.c
+++ src/Rbt.c
@@ -2,28 +2,139 @@
  * @file Rbt.c
  */
 #include "Rbt.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static RBNode *create_node(const Element element);
+
+static void recursive_free(RBNode *rbNode);
+
+static int recursive_search_element(const RBNode *rbNode, const Element element);
+
+static void write_node_in_file(const RBNode *rbNode, FILE *file);
+
+static void recursive_write_digraph(const RBNode *noeud, FILE *file);
+
 
 void initialize_rbtree(RBTree *rbTree)
 {
-
+    rbTree->root = NULL;
 }
 
 void free_rbtree(RBTree *rbTree)
 {
-
+    recursive_free(rbTree->root);
 }
+
 
 void insert_element_in_rbtree(RBTree *rbTree, const Element element)
 {
-
+    if (rbTree->root == NULL)
+    {
+        rbTree->root = create_node(element);
+        rbTree->root->color = BLACK;
+    } else
+    {
+        insert_element_in_rbtree(rbTree, element);
+        /* TODO : rotation*/
+    }
 }
 
 int search_element_in_rbtree(const RBTree *rbTree, const Element element)
 {
-    return 0;
+    return recursive_search_element(rbTree->root, element);
 }
 
 void create_dot_file_for_rbtree(const RBTree *rbTree, const char *fileName)
 {
+    FILE *fichierDigraph;
+    fichierDigraph = fopen(fileName, "w");
 
+    fprintf(fichierDigraph, "strict digraph AVL {\n node [style=filled fontcolor=white] \n");
+    fprintf(fichierDigraph, "%d [fillcolor=black]", rbTree->root->data);
+    recursive_write_digraph(rbTree->root, fichierDigraph);
+    fprintf(fichierDigraph, "}\n");
+    fclose(fichierDigraph);
+}
+
+static RBNode *create_node(const Element element)
+{
+    RBNode *rbNode = malloc(sizeof(RBNode));
+    rbNode->data = element;
+    rbNode->color = RED;
+    rbNode->father = NULL;
+    rbNode->leftChild = NULL;
+    rbNode->rightChild = NULL;
+
+    return rbNode;
+}
+
+static void recursive_free(RBNode *rbNode)
+{
+    if (rbNode != NULL)
+    {
+        recursive_free(rbNode->leftChild);
+        recursive_free(rbNode->rightChild);
+        free(rbNode);
+    }
+}
+
+static int recursive_search_element(const RBNode *rbNode, const Element element)
+{
+    if (rbNode == NULL) return 0;
+    else if (compare_element(element, rbNode->data) > 0) return recursive_search_element(rbNode, element);
+    else if (compare_element(element, rbNode->data) < 0) return recursive_search_element(rbNode, element);
+    else return 1;
+}
+
+static void write_node_in_file(const RBNode *rbNode, FILE *file)
+{
+    static int idnumer = 0;
+    print_element(rbNode->data, file);
+    fprintf(file, "->");
+    fprintf(file, "{ ");
+    if (rbNode->leftChild != NULL || rbNode->rightChild != NULL)
+    {
+
+        if (rbNode->leftChild != NULL)
+        {
+            if (rbNode->leftChild->color == BLACK)
+            {
+                fprintf(file, "%d [fillcolor=black]", rbNode->leftChild->data);
+            } else
+            {
+                fprintf(file, "%d [fillcolor=red]", rbNode->leftChild->data);
+            }
+        }
+        else
+            fprintf(file, "id%d [shape=point]", idnumer++);
+
+
+        fprintf(file, " ");
+        if (rbNode->rightChild != NULL)
+        {
+            if (rbNode->rightChild->color == BLACK)
+            {
+                fprintf(file, "%d [fillcolor=black]", rbNode->rightChild->data);
+            } else
+            {
+                fprintf(file, "%d [fillcolor=red]", rbNode->rightChild->data);
+            }
+        }
+        else
+            fprintf(file, "id%d [shape=point]", idnumer++);
+    }
+
+
+    fprintf(file, "};\n");
+}
+
+static void recursive_write_digraph(const RBNode *noeud, FILE *file)
+{
+    if (noeud != NULL)
+    {
+        write_node_in_file(noeud, file);
+        recursive_write_digraph(noeud->leftChild, file);
+        recursive_write_digraph(noeud->rightChild, file);
+    }
 }
