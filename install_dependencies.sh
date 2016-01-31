45de61cdc8defb2fc65b70504987a792ed351cd1
Mohamed El Mouctar HAIDARA <mohamed.haidara@insa-lyon.fr>
2016-02-01 00:28:44 +0100

    --no-check-certificate for wget

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt-get install -y valgrind
 
 # install cmake 3.4.3
-wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz
+wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz
 tar xf cmake-3.4.3.tar.gz
 cd cmake-3.4.3
 ./configure
