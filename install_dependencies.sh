c956e7c14b55b7277d05622c745d970887d522fc
Mohamed El Mouctar HAIDARA <mohamed.haidara@insa-lyon.fr>
2016-02-01 00:21:50 +0100

    update test_script and travis to install cmake 3.4.3

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# install valgrind
+sudo apt-get install -y valgrind
+
+# install cmake 3.4.3
+wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz
+tar xf cmake-3.4.3.tar.gz
+cd cmake-3.4.3
+./configure
+make
+sudo make install
--- test.sh
+++ test.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-# Build makefile
-cmake .
-
-# build the project
-make
-
-# run unit test
-# make test also works
-ctest
-
-# TODO : memcheck with valgrind
+cmake . && make && ctest
