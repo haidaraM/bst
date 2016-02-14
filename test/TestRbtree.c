ad9f1b4dda80d0fd6d132e5bfa04e3cb0969ed86
Mohamed El Mouctar HAIDARA <mohamed.haidara@insa-lyon.fr>
2016-02-14 22:54:22 +0100

    fix rbtree test

--- test/TestRbtree.c
+++ test/TestRbtree.c
@@ -20,33 +20,39 @@ RBNode *right_rotation(RBTree * rbTree,RBNode *root);
  */
 void test_left_rotation()
 {
-    TypePackage typePackage = get_int_typePackage();
-    RBNode *root = create_node(create_element_int(1));
-    root->left_child = create_node(0);
+    Element element1 = create_element_int(1);
+    RBNode *root = create_node(element1);
+
+    Element element0 = create_element_int(0);
+
+    root->left_child = create_node(element0);
     root->left_child->father = root;
 
-    RBNode *rightNode = create_node(create_element_int(2));
+    Element element2 = create_element_int(2);
+    RBNode *rightNode = create_node(element2);
     root->right_child = rightNode;
     rightNode->father = root;
 
-    rightNode->right_child = create_node(create_element_int(3));
+    Element element3 = create_element_int(3);
+    rightNode->right_child = create_node(element3);
     rightNode->right_child->father = rightNode;
 
 
-    rightNode->right_child->right_child = create_node(create_element_int(4));
+    Element element4 = create_element_int(4);
+    rightNode->right_child->right_child = create_node(element4);
     rightNode->right_child->right_child->father = rightNode->right_child;
 
     RBNode *new_right_child = left_rotation(NULL, rightNode);
     rightNode = NULL;
 
-    assert(new_right_child->data == create_element_int(3));
+    assert(new_right_child->data == element3);
     assert(new_right_child->father == root);
     assert(root->right_child == new_right_child);
 
-    assert(new_right_child->left_child->data == 2);
+    assert(new_right_child->left_child->data == element2);
     assert(new_right_child->left_child->father == new_right_child);
 
-    assert(new_right_child->right_child->data == 4);
+    assert(new_right_child->right_child->data == element4);
     assert(new_right_child->right_child->father == new_right_child);
 
 
@@ -56,6 +62,11 @@ void test_left_rotation()
     free(root->left_child);
     free(root->right_child);
     free(root);
+    free(element0);
+    free(element1);
+    free(element2);
+    free(element3);
+    free(element4);
 }
 
 /**
@@ -74,35 +85,42 @@ void test_left_rotation()
 
 void test_right_rotation()
 {
-    RBNode *root = create_node(1);
-    root->left_child = create_node(0);
+    Element element1 = create_element_int(1);
+    RBNode *root = create_node(element1);
+
+    Element element0 = create_element_int(0);
+
+    root->left_child = create_node(element0);
     root->left_child->father = root;
 
-    RBNode *rightNode = create_node(3);
+    Element  element3 = create_element_int(3);
+
+    RBNode *rightNode = create_node(element3);
     root->right_child = rightNode;
     rightNode->father = root;
 
-    rightNode->right_child = create_node(4);
+    Element element4 = create_element_int(4);
+    rightNode->right_child = create_node(element4);
     rightNode->right_child->father = rightNode;
 
-
-    rightNode->left_child = create_node(2);
+    Element element2 = create_element_int(2);
+    rightNode->left_child = create_node(element2);
     rightNode->left_child->father = rightNode;
 
     RBNode *new_right_child = right_rotation(NULL, rightNode);
     rightNode = NULL;
 
-    assert(new_right_child->data == 2);
+    assert(new_right_child->data == element2);
     assert(new_right_child->father == root);
     assert(root->right_child == new_right_child);
 
     assert(new_right_child->left_child == NULL);
 
-    assert(new_right_child->right_child->data == 3);
+    assert(new_right_child->right_child->data == element3);
     assert(new_right_child->right_child->left_child == NULL);
     assert(new_right_child->right_child->father == new_right_child);
 
-    assert(new_right_child->right_child->right_child->data == 4);
+    assert(new_right_child->right_child->right_child->data == element4);
     assert(new_right_child->right_child->right_child->father == new_right_child->right_child);
 
     free(new_right_child->right_child->right_child);
@@ -110,6 +128,11 @@ void test_right_rotation()
     free(root->left_child);
     free(root->right_child);
     free(root);
+    free_element_int(element0);
+    free_element_int(element1);
+    free_element_int(element2);
+    free_element_int(element3);
+    free_element_int(element4);
 }
 
 void test_get_height()
@@ -119,16 +142,16 @@ void test_get_height()
     initialize_rbtree(&rbTree, &typePackage);
     assert(get_rbtree_height(&rbTree) == 0);
 
-    insert_element_in_rbtree(&rbTree,50);
+    insert_element_in_rbtree(&rbTree,create_element_int(50));
     assert(get_rbtree_height(&rbTree) == 1);
 
-    insert_element_in_rbtree(&rbTree,-1);
+    insert_element_in_rbtree(&rbTree,create_element_int(-1));
     assert(get_rbtree_height(&rbTree) == 2);
 
-    insert_element_in_rbtree(&rbTree,10);
+    insert_element_in_rbtree(&rbTree,create_element_int(10));
     assert(get_rbtree_height(&rbTree) == 2);
 
-    insert_element_in_rbtree(&rbTree,20);
+    insert_element_in_rbtree(&rbTree,create_element_int(20));
     assert(get_rbtree_height(&rbTree) == 3);
 
     free_rbtree(&rbTree);
@@ -136,19 +159,22 @@ void test_get_height()
 }
 
 void test_get_nb_elements()
+
 {
     RBTree rbTree;
     TypePackage typePackage = get_int_typePackage();
     initialize_rbtree(&rbTree, &typePackage);
     assert(get_nb_elements_in_rbtree(&rbTree) == 0);
 
-    insert_element_in_rbtree(&rbTree,14);
+    insert_element_in_rbtree(&rbTree,create_element_int(14));
     assert(get_nb_elements_in_rbtree(&rbTree) == 1);
 
-    insert_element_in_rbtree(&rbTree,14);
+    Element element14 = create_element_int(14);
+    insert_element_in_rbtree(&rbTree,element14);
     assert(get_nb_elements_in_rbtree(&rbTree) == 1);
 
     free_rbtree(&rbTree);
+    free(element14);
 }
 
 int main(int argc, char const *ar[])
